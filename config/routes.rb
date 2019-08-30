73a0b46740a3c7f974fff87ce670064b59110f64
yurifukuchi <uro_409@outlook.jp>
2019-08-31 04:21:08 +0900

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: "users/sessions",
+  }
+
+  resources :users, :only => [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'posts#index'
+  resources :posts, only:[:new,:create,:edit,:update,:destroy]
 end
