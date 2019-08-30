a8311e73f555e6b0c06d78fa12232c31f1c27cfe
yurifukuchi <uro_409@outlook.jp>
2019-08-31 04:21:41 +0900

    Create post_category.sql

---
+++ sql/post_category.sql
@@ -0,0 +1,5 @@
+INSERT INTO `post_categories` (`id`, `name`, `created_at`, `updated_at`)VALUES
+	(1, 'なし', NOW(), NOW()),
+	(2, 'おすすめ', NOW(), NOW()),
+	(3, 'レビュー', NOW(), NOW()),
+	(4, 'フレンド募集', NOW(), NOW());
