b0a58380fcab7b266f912099317839afe2988e4f
yurifukuchi <uro_409@outlook.jp>
2019-08-31 15:50:32 +0900

    create search

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'posts#index'
-  resources :posts, only:[:new,:create,:edit,:update,:destroy]
+  resources :posts, only:[:new,:create,:edit,:update,:destroy] do
+    collection do
+      get 'search'
+    end
+  end
 end
