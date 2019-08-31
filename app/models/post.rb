90f0a5840a3158ea57f01530051ec747152ef7f3
yurifukuchi <uro_409@outlook.jp>
2019-08-31 15:51:45 +0900

    update post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,14 @@ class Post < ApplicationRecord
   belongs_to :post_category
 
   mount_uploader :image, ImageUploader
+
+  paginates_per 30
+
+  def self.search(search)
+    if search
+      where(['text LIKE ?', "%#{search}%"]) 
+    else
+      all
+    end
+  end
 end
