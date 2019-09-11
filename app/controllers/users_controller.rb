b02a77f6301bc142275f12a241a0b2e5036d7a51
yurifukuchi <uro_409@outlook.jp>
2019-09-12 00:42:21 +0900

    update pagenation-view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,14 +2,14 @@ class UsersController < ApplicationController
   before_action :set_user
 
   def show
-    @posts = Post.where(user_id:params[:id]).order("created_at desc")
+    @posts = Post.where(user_id:params[:id]).page(params[:page]).order("created_at desc")
     require "date"
   end
 
   def likes
-    @posts = Post.where(user_id:params[:id]).order("created_at desc")
+    @posts = Post.where(user_id:params[:id])
     require "date"
-    @posts = @user.liked_posts
+    @posts = @user.liked_posts.page(params[:page]).order("created_at desc")
     unless current_user.id == @user.id
       redirect_to root_path
     end
